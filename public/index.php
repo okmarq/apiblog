837435101a49b692d38a5c69b33a74b03c67756e
Joel Okoromi <okmarq@gmail.com>
2022-01-01 23:09:02 +0100

    update

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 //Get Heroku JawsDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
