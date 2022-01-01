41082647302b420963ae9fbc524150ce57f54a57
Joel Okoromi <okmarq@gmail.com>
2022-01-01 22:57:56 +0100

    update

--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
 <?php
 
+//Get Heroku JawsDB connection information
+$jawsdb_url = parse_url(getenv("DATABASE_URL"));
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
+
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
