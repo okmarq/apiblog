5324a21296ac6813362d3e8bbde0fa9ec4b23b2e
Joel Okoromi <okmarq@gmail.com>
2022-01-02 14:44:01 +0100

    tailwindcss form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
-  content: [],
-  theme: {
-    extend: {},
+  purge: {
+    content: [
+      "./public/**/*.html",
+      "./src/**/*.vue",
+      "./assets/**/*.{vue,js,ts,jsx,tsx}",
+      "./templates/**/*.{html,twig}",
+    ]
   },
-  // plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  content: [
+    "./assets/**/*.{vue,js,ts,jsx,tsx}",
+    "./templates/**/*.{html,twig}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+    }
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    // require("@tailwindcss/typography")
+  ],
 }
